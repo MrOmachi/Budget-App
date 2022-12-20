98145b004c9335027aba1173dc8e1079b5e75f52
Omachi John <omachi.john@yahoo.com>
2022-12-20 09:19:37 -0800

    migrate purchase db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_19_200123) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_20_171706) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_19_200123) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "purchases", force: :cascade do |t|
+    t.string "name"
+    t.integer "amount"
+    t.bigint "category_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_purchases_on_category_id"
+    t.index ["user_id"], name: "index_purchases_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +49,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_19_200123) do
   end
 
   add_foreign_key "categories", "users"
+  add_foreign_key "purchases", "categories"
+  add_foreign_key "purchases", "users"
 end
